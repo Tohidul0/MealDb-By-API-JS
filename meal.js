ae29bf912552116d7adf3607d81a1b6b2c15592a
Tohidul0 <tohidulalam967418@gmail.com>
2023-11-29 00:07:15 +0600

    search part hendle done also remove serch part when again search it also two hidden part ad when invalid or empty string given its working

--- meal.js
+++ meal.js
@@ -1,35 +1,61 @@
 const loadData = () => {
     const input = document.getElementById('search-input');
     const inputText = input.value;
-    url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputText}`
-    fetch(url)
+    const show1 = document.getElementById('write');
+    show1.innerText = '';
+    if(inputText == ''){
+       
+        const div=document.createElement('div');
+        div.innerHTML =`<h2 class="text-center  w-50 mx-auto">Write something</h2>`;
+        show1.appendChild(div);
+
+        
+    }
+    else{
+        url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputText}`
+        fetch(url)
         .then( res => res.json())
         .then( data => showData(data))
+    }
+    
 }
 
 
 
 const showData = (meals) =>{
     console.log(meals);
-    const all =meals.meals;
-    for( const a of all){
-
-        const meal = a;
     const mealItem = document.getElementById('meal-item');
-    const div = document.createElement('div');
-    div.classList.add('col')
-    div.innerHTML =
-    `
-        <div   style="width: 18rem;" >
-            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${meal.strMeal}</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    `
-    mealItem.appendChild(div);
+    mealItem.innerHTML = '';
+    
+    if(meals.meals && meals.meals.length){        
+
+        const all =meals.meals;
+        for( const a of all){
+
+            const meal = a;
+            const div = document.createElement('div');
+            div.classList.add('col')
+            div.innerHTML =
+            `
+                <div   style="width: 18rem;" >
+                    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${meal.strMeal}</h5>
+                      <p class="card-text">${meal.strInstructions.slice(0 , 200)}</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            `
+            mealItem.appendChild(div);
+        }
+
+  
+    }
+    else{
+        const show1 = document.getElementById('write');
+        const div=document.createElement('div');
+        div.innerHTML =`<h2 class="text-center  w-50 mx-auto">Give a True meal name plz...!</h2>`;
+        show1.appendChild(div);
     }
     
     
