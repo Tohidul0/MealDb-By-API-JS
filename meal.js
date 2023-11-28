b04db2ab8f51a34a49c0ba3a705efc34cb22dfc7
Tohidul0 <tohidulalam967418@gmail.com>
2023-11-29 01:27:33 +0600

    animation + hendle + responsive

--- meal.js
+++ meal.js
@@ -4,10 +4,15 @@ const loadData = () => {
     const show1 = document.getElementById('write');
     show1.innerText = '';
     if(inputText == ''){
-       
+        // if(m!=0){
+        //     //m.innerHTML='';
+        //     m=0;
+        // }
         const div=document.createElement('div');
-        div.innerHTML =`<h2 class="text-center  w-50 mx-auto">Write something</h2>`;
+        div.innerHTML =`<h2 class=" omg text-danger text-center  w-50 mx-auto">Write something</h2>`;
         show1.appendChild(div);
+        const s = document.getElementById('meal-item');
+        s.innerHTML = '';
 
         
     }
@@ -27,6 +32,7 @@ const showData = (meals) =>{
     const mealItem = document.getElementById('meal-item');
     mealItem.innerHTML = '';
     
+    
     if(meals.meals && meals.meals.length){        
 
         const all =meals.meals;
@@ -37,7 +43,7 @@ const showData = (meals) =>{
             div.classList.add('col')
             div.innerHTML =
             `
-                <div   style="width: 18rem;" >
+                <div class="mx-auto"  style="width: 20rem;" >
                     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">${meal.strMeal}</h5>
@@ -54,9 +60,11 @@ const showData = (meals) =>{
     else{
         const show1 = document.getElementById('write');
         const div=document.createElement('div');
-        div.innerHTML =`<h2 class="text-center  w-50 mx-auto">Give a True meal name plz...!</h2>`;
+        div.innerHTML =`<h2 class="  omg text-danger text-center  w-50 mx-auto">Give a True meal name plz...!</h2>`;
         show1.appendChild(div);
     }
     
     
 }
+
+
